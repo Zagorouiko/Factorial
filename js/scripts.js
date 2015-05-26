fbc7959cad2e832de7386ba5f1aa80ccb2463763
ian bevel & Leon Zagorouiko <student@epicodus.com>
2015-05-26 13:48:06 -0700

    have completed logic

---
+++ js/scripts.js
@@ -0,0 +1,24 @@
+function factorial(number) {
+  if (number === 0) {
+    return 1;
+  } else {
+    return (number * factorial(number - 1));
+  }
+};
+
+
+
+
+
+
+
+
+//   if (number > 1) {
+//     var total = 1
+//     for (i > 1; i === number; i++) {
+//       total *= i}
+//       return total;
+//   } else ((number === 0) || (number === 1)) {
+//     return 1;
+//   }
+// };
